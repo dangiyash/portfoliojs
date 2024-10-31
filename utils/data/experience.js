0e454bf8a41f51bd95bb090a643f392df40ebf52
YASH <155509845+dangiyash@users.noreply.github.com>
2024-10-31 20:23:42 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,18 +2,18 @@ export const experiences = [
   {
     id: 2,
     title: "Web Dev @ ChESA",
-    company: "ChESA MANIT",
+    company: "ChESA is the offical association of Chemical Engineering students of MANIT.",
     duration: "Aug 2024 - Present"
   },
   {
     id: 2,
     title: "Manager @ Tooryanaad",
-    company: "Tooryanaad",
+    company: "Tooryanaad, Central India's Largest Hindi Fest!",
     duration: "March 2024 - Present"
   },
   {
     id: 3,
-    title: "Self Employed",
+    title: "Learner",
     company: "Code and build something in everyday.",
     duration: "Jan 2024 - Present"
   }
