a3b13c49cb94e9c8969c60282ad0899ac293eae5
YASH <155509845+dangiyash@users.noreply.github.com>
2024-10-31 20:20:11 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -4,17 +4,5 @@ export const educations = [
     title: "B. Tech Chem. Engg.",
     duration: "2023 - Present",
     institution: "National Institute of Technology, Bhopal (MANIT)",
-  },
-  {
-    id: 2,
-    title: "Cracked JEE MAINS",
-    duration: "2022",
-    
-  },
-  {
-    id: 3,
-    title: "High & Higher School Certificate",
-    duration: "2020 - 2022",
-    institution: "",
   }
 ]
